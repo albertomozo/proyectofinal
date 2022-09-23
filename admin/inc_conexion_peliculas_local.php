d1fdc6ea4644ef8c7f37ad3f784f2f09dbdddbbf
Alberto Mozo <79279132+albertomozo@users.noreply.github.com>
2022-09-23 13:01:50 +0200

    Delete inc_conexion_peliculas_local.php

--- admin/inc_conexion_peliculas_local.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servidor="localhost";
-$bduser="root";
-$bdclave="";
-$bdnombre="peliculas";
-$conpel=mysqli_connect($servidor,$bduser,$bdclave,$bdnombre);
-$tmdb_apikey='98fee347b91da83932ea8b9daa0edece';
-$tmdb_ruta_poster = 'https://image.tmdb.org/t/p/w154'; // 
-?>
