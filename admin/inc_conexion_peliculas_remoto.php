c2a0cc3a9f76435c9a494212bcf8607c1e73a73b
Alberto Mozo <79279132+albertomozo@users.noreply.github.com>
2022-09-23 13:01:24 +0200

    Delete inc_conexion_peliculas_remoto.php

--- admin/inc_conexion_peliculas_remoto.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servidor="sql305.epizy.com";
-$bduser="epiz_30918323";
-$bdclave="MI6hODcUFSm";
-$bdnombre="epiz_30918323_peliculas";
-$conpel=mysqli_connect($servidor,$bduser,$bdclave,$bdnombre);
-$tmdb_apikey='98fee347b91da83932ea8b9daa0edece';
-$tmdb_ruta_poster = 'https://image.tmdb.org/t/p/w154'; // 
-?>
