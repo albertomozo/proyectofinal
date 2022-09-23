b721d7ba7de6e26a770a2d69ad8f733129497f4c
Alberto Mozo <79279132+albertomozo@users.noreply.github.com>
2022-09-23 13:03:19 +0200

    Update inc_conexion_peliculas.php

--- admin/inc_conexion_peliculas.php
+++ admin/inc_conexion_peliculas.php
@@ -4,6 +4,6 @@ $bduser="root";
 $bdclave="";
 $bdnombre="peliculas";
 $conpel=mysqli_connect($servidor,$bduser,$bdclave,$bdnombre);
-$tmdb_apikey='98fee347b91da83932ea8b9daa0edece';
+$tmdb_apikey='*************'; // clave obtenida al registrasrse en https://www.themoviedb.org/ 
 $tmdb_ruta_poster = 'https://image.tmdb.org/t/p/w154'; // 
 ?>
